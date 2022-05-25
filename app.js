a3bb3482033885226467bc13c69d0dcd1340707f
Aaron Vigil <aaronvigil96@gmail.com>
2022-05-25 19:34:21 -0300

    Trayendo datos de una api con promesas

--- app.js
+++ app.js
@@ -1,2 +1,26 @@
-alert('hola soy una alerta');
-alert('Soy una segunda alerta en un script externo de JS');
+window.addEventListener('DOMContentLoaded', () => {
+    cargarPersonas();
+})
+
+const cargarPersonas = async () => {
+    try {
+        const response = await fetch('https://fakerapi.it/api/v1/users?_quantity=100');
+
+        if(response.status === 200){
+            const datos = await response.json();
+
+            let personas = '';
+
+            datos.data.forEach(persona => {
+                personas += `<p>
+                Nombre: ${persona.lastname} ${persona.firstname}</p>`
+            })
+            document.querySelector('#contenedor').innerHTML = personas;
+
+        } else {
+            console.log('Estas teniendo un error')
+        }
+    } catch (error){
+        console.log(error)
+    }
+}
